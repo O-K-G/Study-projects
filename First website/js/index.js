07b8c2bda09bc08fd4d63ee582b6134d124a8c48
O-K-G <55091190+O-K-G@users.noreply.github.com>
2020-05-09 14:27:38 +0300

    Update index.js

--- First website/js/index.js
+++ First website/js/index.js
@@ -28,11 +28,6 @@ function showHideNav() {
   }
 }
 
-// Start of Typeform embeded code.
-
-(function () { var qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement, gt = d.getElementsByTagName, id = "typef_orm", b = "https://embed.typeform.com/"; if (!gi.call(d, id)) { js = ce.call(d, "script"); js.id = id; js.src = b + "embed.js"; q = gt.call(d, "script")[0]; q.parentNode.insertBefore(js, q) } })()
-
-// End of Typeform embeded code.
 
 function switchDayCache() {
   var x = localStorage.getItem("firstDay");
